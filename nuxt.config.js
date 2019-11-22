876134e2c77c80cc937e97cfc7697b100c4c281e
yoonjoohye <67yjh00@gmail.com>
2019-11-22 20:26:24 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,12 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'musinsa',
+    title: '무신사',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '무신사 스토어 UI/UX 리뉴얼' }
+      { hid: 'description', name: 'description', content: '무신사 스토어 UI/UX 리뉴얼' },
+      { name:'keywords', content:'무신사, 스토어, 쇼핑, 스트릿, 패션'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
