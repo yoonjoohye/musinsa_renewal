fd56ae3c6150dd23f1bd9700f77584cd9b186dd9
yoonjoohye <67yjh00@gmail.com>
2019-12-01 19:15:22 +0900

    신규 입점 브랜드 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <section class="w-screen min-h-screen flex justify-center">
     <div class="container py-10">
-      <div class="flex justify-center py-10">
-        <div class="w-1/2 p-5 border-r">
-          <div class="flex justify-center justify-between mb-8">
-            <div class="text-2xl font-light">디자이너</div>
-            <div class="text-gray-500">전체보기 ></div>
-          </div>
-          <div class="flex flex-wrap justify-between">
-            <Designer v-for="x in 5" v-bind:key="x"></Designer>
-          </div>
-        </div>
-        <div class="w-1/2 p-5">
-          <div class="flex justify-center justify-between mb-8">
-            <div class="text-2xl font-light">브랜드</div>
-            <div class="text-gray-500">전체보기 ></div>
-          </div>
-          <div class="flex flex-wrap justify-between">
-            <Brand v-for="x in 8" v-bind:key="x" :index="x"></Brand>
-          </div>
-        </div>
-      </div>
-
       <div class="border-b py-10">
         <div class="flex justify-center mb-8">
           <div class="flex justify-center w-full text-2xl font-light">타임세일 <img class="w-5 ml-1" src="../assets/image/icon/time.svg"></div>
@@ -49,6 +28,21 @@
       <div class="border-b py-10">
         <div class="flex justify-center mb-8">
           <div class="w-full flex flex-col items-center">
+            <div class="text-2xl text-center font-light">신규 입점 브랜드</div>
+          </div>
+          <span class="text-gray-500 w-20">보러가기 ></span>
+        </div>
+        <div class="flex justify-center justify-between">
+          <div class="flex flex-col justify-center items-center" v-for="x in 5" v-bind:key="x">
+            <Brand :index="x"></Brand>
+            <span class="border rounded px-2 py-1">응원하기</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="border-b py-10">
+        <div class="flex justify-center mb-8">
+          <div class="w-full flex flex-col items-center">
             <div class="text-2xl font-light">무신사 추천</div>
             <div class="flex mb-2 font-light text-gray-500">
               <div class="px-2 py-1 mr-3 cursor-pointer underline">스태프</div>
@@ -62,6 +56,27 @@
         </div>
         <div class="masonry">
           <Style v-for="x in 6" v-bind:key="x" :index="x"></Style>
+        </div>
+      </div>
+
+      <div class="flex justify-center py-10">
+        <div class="w-1/2 p-5 border-r">
+          <div class="flex justify-center justify-between mb-8">
+            <div class="text-2xl font-light">디자이너</div>
+            <div class="text-gray-500">전체보기 ></div>
+          </div>
+          <div class="flex flex-wrap justify-between">
+            <Designer v-for="x in 5" v-bind:key="x"></Designer>
+          </div>
+        </div>
+        <div class="w-1/2 p-5">
+          <div class="flex justify-center justify-between mb-8">
+            <div class="text-2xl font-light">브랜드</div>
+            <div class="text-gray-500">전체보기 ></div>
+          </div>
+          <div class="flex flex-wrap justify-between">
+            <Brand v-for="x in 8" v-bind:key="x" :index="x"></Brand>
+          </div>
         </div>
       </div>
 
